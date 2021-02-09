b298bcccd3960d21d073e08b26aa87ae8db1aaf9
maryna1408 <cabanmarina9@gmail.com>
2021-02-09 14:03:21 +0200

    Cards

--- js/app.js
+++ js/app.js
@@ -28,20 +28,20 @@ renderCards(CARS, cardListEl)
 
 
 function renderCards(data_array, node) {
-    let html = ''
+  let html = ''
 
-    //    for (let i = 0; i < data_array.length; i++) {
-    //        const element = data_array[i];
-    //        html += createCardHTML(element)
-    //    }
-    data_array.forEach(el => html += createCardHTML(el));
+  //    for (let i = 0; i < data_array.length; i++) {
+  //        const element = data_array[i];
+  //        html += createCardHTML(element)
+  //    }
+  data_array.forEach(el => html += createCardHTML(el));
 
-    node.innerHTML = html
+  node.innerHTML = html
 }
 
 
 function createCardHTML(card_data) {
-    return `<div class="col card mb-3">
+  return `<div class="col card mb-3">
     <div class="row g-0">
       <div class="col-4">
         <img class="card-img" width="1" height="1" loading="lazy" src="${card_data.img}" alt="${card_data.make} ${card_data.model}" />
@@ -50,7 +50,23 @@ function createCardHTML(card_data) {
         <div class="card-body">
           <h5 class="card-title">${card_data.make} ${card_data.model} ${card_data.engine_volume} ${card_data.transmission} (${card_data.year})</h5>
           <h6 class="card-price">${card_data.price}$</h6>
-          <p class="card-vin">${card_data.vin}</p>
+          <div class="col-8 parameters">
+          <span class="span-parameters"><i class="fas fa-tachometer-alt"></i> ${card_data.odo} km</span>
+          <span class="span-parameters"><i class="fas fa-map-marker-alt"></i> ${card_data.country}</span>
+          <span class="span-parameters"><i class="fas fa-gas-pump"></i> ${card_data.fuel}, ${card_data.engine_volume}</span>
+          <span class="span-parameters">${card_data.transmission}</span>
+          </div>
+          <article class="content-article"> Fuel Consumption (l/1000km)
+          <section><i class="fas fa-city"></i> ${card_data.consume.city}</section>
+          <section><i class="fas fa-road"></i> ${card_data.consume.road}</section>
+          <section><i class="fas fa-sync"></i> ${card_data.consume.mixed}</section>
+          </article>
+          <p class="card-vin"><span class="vin-span">VIN:</span> ${card_data.vin || "none"}</p>
+          <p>Color: ${card_data.color}</p>
+          <div class="call">
+          <button class="call-button"><i class="fas fa-phone-alt"></i> Call</button>
+          <p><i class="far fa-user"></i> ${card_data.seller}</p>
+          </div>
         </div>
       </div>
       <div class="col-12 card-footer text-muted">
@@ -61,10 +77,10 @@ function createCardHTML(card_data) {
   </div>`
 }
 
-function sum(a, b) {
-    return a + b
-}
+// function sum(a, b) {
+//     return a + b
+// }
 
-let result = 0
+// let result = 0
 
-result = sum(10, 5)
+// result = sum(10, 5)
